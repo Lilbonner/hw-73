e15a7f94510f47af18a73a4ff47ab3873ae65db0
Daniyar Nasirov <lilbonnie666@gmail.com>
2024-03-15 00:58:59 +0600

    complete second task

--- index.js
+++ index.js
@@ -7,6 +7,20 @@ app.get('/:message', (req, res) => {
     res.send(message);
 });
 
+const Vigenere = require('vigenere')
+
+app.get('/encode/:password',(req,res) => {
+    const password = req.params.password;
+    const encodePassword = Vigenere.encode(password, 'hax0rk3y')
+    res.send(encodePassword)
+})
+
+app.get('/decode/:password',(req,res) => {
+    const password = req.params.password;
+    const decodePassword = Vigenere.decode(password, 'hax0rk3y')
+    res.send(decodePassword)
+})
+
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`)
 })
